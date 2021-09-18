3f2ada71d62ff2903b13d7a8e9d8e4e9d04d51c7
Greg Maj <gregory.j.maj@gmail.com>
2021-09-18 17:35:46 -0600

    deciding on tkinter for application.

--- index.py
+++ index.py
@@ -1,5 +1,20 @@
-print('hello world')
+import tkinter as tk
 
-print('new edit')
-print('new edit')
-print('new edit')
+def donothing():
+    ...
+
+window = tk.Tk()
+
+menu_bar = tk.Menu(window)
+file_menu = tk.Menu(menu_bar, tearoff=0)
+file_menu.add_command(label="New", command=donothing)
+file_menu.add_command(label="Open", command=donothing)
+file_menu.add_command(label="Save", command=donothing)
+file_menu.add_separator()
+file_menu.add_command(label="Exit", command=window.quit)
+menu_bar.add_cascade(label="File", menu=file_menu)
+
+
+
+window.config(menu=menu_bar)
+window.mainloop()
