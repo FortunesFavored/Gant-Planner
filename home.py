60efe2edbac9dd05d65c72d4608472c25de8254a
Greg Maj <gregory.j.maj@gmail.com>
2021-09-20 15:33:45 -0600

    Added working timer button.

--- home.py
+++ home.py
@@ -1,5 +1,7 @@
 import tkinter as tk
-from datetime import datetime
+from datetime import datetime, time
+import random
+
 
 class MainWindow:
 
@@ -7,38 +9,75 @@ class MainWindow:
         self.name = name
         self.height = height
         self.width = width
-        self.start_time = None
+        self.running = True
+        self.CreateWindow()
 
     def CreateWindow(self):
-        window = tk.Tk()
-        window.title(self.name)
-        window.geometry(f"{self.height}x{self.width}")
+        self.window = tk.Tk()
+        self.window.title(self.name)
+        self.window.geometry(f"{self.height}x{self.width}")
 
-        self.CreateMenuBar(window)
+        self.CreateMenuBar()
 
-        button = tk.Button(window,text="Make a second window", command=self.donothing)
-        button.pack()
+        self.lbl_result = tk.Label()
+        self.label_a = tk.Label(master=self.window)
+        self.button1 = tk.Button(master=self.window,text="Start the clock", command=self.StartRunning)
 
-        window.mainloop()
+        self.proj_lst_frame = tk.Frame(self.window)
 
-    def CreateMenuBar(self, window):
-        menu_bar = tk.Menu(window)
-        file_menu = tk.Menu(menu_bar, tearoff=0)
-        file_menu.add_command(label="New", command=self.donothing)
-        file_menu.add_command(label="Open", command=self.donothing)
-        file_menu.add_command(label="Save", command=self.donothing)
-        file_menu.add_separator()
-        file_menu.add_command(label="Exit", command=window.quit)
-        view_menu = tk.Menu(menu_bar, tearoff=0)
-        view_menu.add_command(label="Timelines")
-        menu_bar.add_cascade(label="File", menu=file_menu)
-        menu_bar.add_cascade(label="View", menu=view_menu)
-        window.config(menu=menu_bar)
+
+        self.button = tk.Button(master=self.proj_lst_frame,text="Make a second self.window", command=self.donothing)
+
+        self.button1.pack()
+        self.lbl_result.pack()
+
+        self.proj_lst_frame.pack()
+        self.button.pack()
+        self.label_a.pack()
+
+        self.window.mainloop()
+
+    def CreateMenuBar(self):
+        self.menu_bar = tk.Menu(self.window)
+        self.file_menu = tk.Menu(self.menu_bar, tearoff=0)
+        self.file_menu.add_command(label="New", command=self.donothing)
+        self.file_menu.add_command(label="Open", command=self.donothing)
+        self.file_menu.add_command(label="Save", command=self.donothing)
+        self.file_menu.add_separator()
+        self.file_menu.add_command(label="Exit", command=self.window.quit)
+        self.view_menu = tk.Menu(self.menu_bar, tearoff=0)
+        self.view_menu.add_command(label="Timelines")
+        self.menu_bar.add_cascade(label="File", menu=self.file_menu)
+        self.menu_bar.add_cascade(label="View", menu=self.view_menu)
+        self.window.config(menu=self.menu_bar)
+
+    def StartRunning(self):
+        self.running = True
+        self.start_time = datetime.now()
+        self.StartTimer()
+
+    def StartTimer(self):
+        if self.running == True:
+            self.current_time = datetime.now()
+            # print(type(self.current_time))
+            # print(type(self.start_time))
+            # print(self.current_time)
+            # print(self.start_time)
+            self.button1.config(text="Stop the clock", command=self.EndTimer)
+            self.lbl_result.config(text= self.current_time - self.start_time)
+            self.lbl_result.after(10, self.StartTimer)
+
+        # self.lbl_result['text'] = datetime.now()
+
+    def EndTimer(self):
+        self.running = False
+        self.end_time = datetime.now()
+        self.lbl_result['text'] = self.end_time - self.start_time
+        self.button1.config(text="Start the clock", command=self.StartRunning)
 
     def donothing(self):
-        if self.start_time == None:
+        if self.start_time == 0:
             self.start_time = datetime.now()
         else:
             self.end_time = datetime.now()
-            print((self.end_time - self.start_time))
-            self.start_time = None
+            self.start_time = 0
--- main.py
+++ main.py
@@ -2,4 +2,3 @@ import home
 
 if __name__ == "__main__":
     new_app = home.MainWindow("Gant Planner", 400, 200)
-    new_app.CreateWindow()
